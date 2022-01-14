13f15f257505c215fd3b6db44e988331298d8102
Aamir Shazhad <20058979+aamirshahzadxerobug@users.noreply.github.com>
2022-01-14 13:26:10 +0500

    helper

---
+++ src/helpers.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+if (!function_exists('dd')) {
+    function dd($data)
+    {
+        echo "<pre>";
+        print_r($data);
+        exit;
+        /*array_map(function ($param) {
+            print_r($param);
+        }, func_get_args());
+        die(1);*/
+    }
+}
